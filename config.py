ddcba64c163a08cdd5c504b320050cb7c1fa0f5a
yjyapple <100333278+yjyapple@users.noreply.github.com>
2022-03-25 11:02:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.c4cekt7vkxka.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "bait3273"
+custompass = "Bait3273"
 customdb = "employee"
 custombucket = "yapjiaying-employee"
 customregion = "us-east-1"
