ef27410a2675f8946d816f996d5e971fbf6a2f5a
yjyapple <100333278+yjyapple@users.noreply.github.com>
2022-03-26 15:08:47 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -4,6 +4,8 @@ import os
 import boto3
 from config import *
 
+
+
 app = Flask(__name__)
 
 bucket = custombucket
@@ -18,12 +20,12 @@ db_conn = connections.Connection(
 
 )
 output = {}
-table = 'employee'
+table = 'department'
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('attendance.html')
 
 
 @app.route("/about", methods=['POST'])
@@ -31,70 +33,66 @@ def about():
     return render_template('www.intellipaat.com')
 
 
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
+@app.route("/addAttend", methods=['POST'])
+def AddAttend():
+    attendance_ID = request.form['attendance_ID']
+    emp_ID = request.form['emp_ID']
+    attendance_date = request.form['attendance_date']
+    attendance_status = request.form['attendance_status']
+    
+    
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO attendance VALUES (%s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
-    if emp_image_file.filename == "":
-        return "Please select a file"
 
     try:
 
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+        cursor.execute(insert_sql, (attendance_ID, emp_id, attendance_date, attendance_status))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
+        
     finally:
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+    return render_template('attendance.html')
+    
+ 
    
-
-
-   
+  
 @app.route("/fetchdata", methods=['POST'])
 def fetchdata():
-    select_employee_query = "SELECT emp_id,first_name,last_name,pri_skill,location FROM employee"
-    with connections.cursor() as cursor:
+   
+    select_employee_query = "SELECT * FROM attendance"
+    cursor = db_conn.cursor():
+    
     cursor.execute(select_employee_query)
+    db_conn.commit():
     result = cursor.fetchall()
     for row in result:
         print(row)
-    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location)
+    return render_template('GetEmpOutput.html', attendance_ID=attendance_ID, emp_id=emp_id, attendance_date=attendance_date, attendance_status=attendance_status)
 
+@app.route("/showData", methods=['POST'])
+def showData():
 
+    attendance_ID = request.form['attendance_ID']
+    select_employee_query = "SELECT * FROM attendance WHERE attendance_ID=%s"
+    cursor = db_conn.cursor():
+    
+    cursor.execute(select_employee_query,(attendance_ID))
+    db_conn.commit():
+    
+    for i in cursor:
+       attendance_ID = i[0]
+       emp_id = i[1]
+       attendance_date = i[2]
+       attendance_status = i[3]
+       
+    cursor.close()   
+    return render_template('GetEmpOutput.html', attendance_ID=attendance_ID, emp_id=emp_id, attendance_date=attendance_date, attendance_status=attendance_status)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
